28a1c4d918253f4885c669f20497541154fbb832
VenuMadhav <>
2020-08-01 17:09:52 -0700

    Formatting code with 2 space tab style

--- src/js/domEvents.js
+++ src/js/domEvents.js
@@ -1,97 +1,97 @@
 if (window.browser) {
-    webEventConsumer = window.browser; // firefox
+  webEventConsumer = window.browser; // firefox
 } else {
-    webEventConsumer = window.chrome; // chrome
+  webEventConsumer = window.chrome; // chrome
 }
 
 webEventConsumer.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        details.callerName = "onBeforeRequest";
-        details.requestIdEnhanced = details.requestId;
-        eventTracker.logRequestDetails(details);
-    }, {
-        urls: ["<all_urls>"]
-    }, ["requestBody"]
+  function(details) {
+    details.callerName = "onBeforeRequest";
+    details.requestIdEnhanced = details.requestId;
+    eventTracker.logRequestDetails(details);
+  }, {
+    urls: ["<all_urls>"]
+  }, ["requestBody"]
 );
 
 webEventConsumer.webRequest.onBeforeSendHeaders.addListener(
-    function(details) {
-        details.callerName = "onBeforeSendHeaders";
-        details.requestIdEnhanced = details.requestId;
-        eventTracker.logRequestDetails(details);
-    }, {
-        urls: ["<all_urls>"]
-    }, ["requestHeaders"]
+  function(details) {
+    details.callerName = "onBeforeSendHeaders";
+    details.requestIdEnhanced = details.requestId;
+    eventTracker.logRequestDetails(details);
+  }, {
+    urls: ["<all_urls>"]
+  }, ["requestHeaders"]
 );
 
 webEventConsumer.webRequest.onSendHeaders.addListener(
-    function(details) {
-        details.callerName = "onSendHeaders";
-        details.requestIdEnhanced = details.requestId;
-        eventTracker.logRequestDetails(details);
-    }, {
-        urls: ["<all_urls>"]
-    }, ["requestHeaders"]
+  function(details) {
+    details.callerName = "onSendHeaders";
+    details.requestIdEnhanced = details.requestId;
+    eventTracker.logRequestDetails(details);
+  }, {
+    urls: ["<all_urls>"]
+  }, ["requestHeaders"]
 );
 
 webEventConsumer.webRequest.onHeadersReceived.addListener(
-    function(details) {
-        details.callerName = "onHeadersReceived";
-        details.requestIdEnhanced = details.requestId;
-        eventTracker.logRequestDetails(details);
-    }, {
-        urls: ["<all_urls>"]
-    }, ["responseHeaders"]
+  function(details) {
+    details.callerName = "onHeadersReceived";
+    details.requestIdEnhanced = details.requestId;
+    eventTracker.logRequestDetails(details);
+  }, {
+    urls: ["<all_urls>"]
+  }, ["responseHeaders"]
 );
 
 webEventConsumer.webRequest.onAuthRequired.addListener(
-    function(details) {
-        details.callerName = "onAuthRequired";
-        details.requestIdEnhanced = details.requestId;
-        eventTracker.logRequestDetails(details);
-    }, {
-        urls: ["<all_urls>"]
-    }, ["responseHeaders"]
+  function(details) {
+    details.callerName = "onAuthRequired";
+    details.requestIdEnhanced = details.requestId;
+    eventTracker.logRequestDetails(details);
+  }, {
+    urls: ["<all_urls>"]
+  }, ["responseHeaders"]
 );
 
 webEventConsumer.webRequest.onBeforeRedirect.addListener(
-    function(details) {
-        details.callerName = "onBeforeRedirect";
-        details.requestIdEnhanced = details.requestId;
-        eventTracker.logRequestDetails(details);
-    }, {
-        urls: ["<all_urls>"]
-    }, ["responseHeaders"]
+  function(details) {
+    details.callerName = "onBeforeRedirect";
+    details.requestIdEnhanced = details.requestId;
+    eventTracker.logRequestDetails(details);
+  }, {
+    urls: ["<all_urls>"]
+  }, ["responseHeaders"]
 );
 
 webEventConsumer.webRequest.onResponseStarted.addListener(
-    function(details) {
-        details.callerName = "onResponseStarted";
-        details.requestIdEnhanced = details.requestId;
-        eventTracker.logRequestDetails(details);
-    }, {
-        urls: ["<all_urls>"]
-    }, ["responseHeaders"]
+  function(details) {
+    details.callerName = "onResponseStarted";
+    details.requestIdEnhanced = details.requestId;
+    eventTracker.logRequestDetails(details);
+  }, {
+    urls: ["<all_urls>"]
+  }, ["responseHeaders"]
 );
 
 webEventConsumer.webRequest.onCompleted.addListener(
-    function(details) {
-        details.callerName = "onCompleted";
-        details.requestIdEnhanced = details.requestId;
-        eventTracker.logRequestDetails(details);
-    }, {
-        urls: ["<all_urls>"]
-    }, ["responseHeaders"]
+  function(details) {
+    details.callerName = "onCompleted";
+    details.requestIdEnhanced = details.requestId;
+    eventTracker.logRequestDetails(details);
+  }, {
+    urls: ["<all_urls>"]
+  }, ["responseHeaders"]
 );
 
 webEventConsumer.webRequest.onErrorOccurred.addListener(
-    function(details) {
-        details.callerName = "onErrorOccurred";
-        details.requestIdEnhanced = details.requestId;
-        eventTracker.logRequestDetails(details);
-    }, {
-        urls: ["<all_urls>"]
-    }
+  function(details) {
+    details.callerName = "onErrorOccurred";
+    details.requestIdEnhanced = details.requestId;
+    eventTracker.logRequestDetails(details);
+  }, {
+    urls: ["<all_urls>"]
+  }
 );
 
 
