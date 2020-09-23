5d9cdf9f6d77e685e4ac6b087f383957febc512f
Venu Madhav <>
2020-09-23 11:33:48 -0700

    Adding options page to preserve fixed setting across sessions

--- src/js/detectBrowser.js
+++ src/js/detectBrowser.js
@@ -1,4 +1,5 @@
 // this cannot be used in openHttpTracker.js as that js itself is self implemented and does not take external script files
+
 // But when ever this file is changed, make sure the same copy is updated there and vice-versa
 if (window.browser) {
   httpTracker = {
@@ -11,4 +12,20 @@ if (window.browser) {
     webEventConsumer: window.chrome,
     browserName: "chrome"
   };
+}
+
+function onError(e) {
+  console.error(e);
+}
+
+function getStoredDetails(details) {
+  if (httpTracker.webEventConsumer.runtime.lastError) {
+    onError(httpTracker.webEventConsumer.runtime.lastError);
+  } else {
+    let existingValues = "";
+    if (details.httpTrackerGlobalExcludePatterns) {
+      existingValues = details.httpTrackerGlobalExcludePatterns.join(", ");
+    }
+    return existingValues;
+  }
 }
